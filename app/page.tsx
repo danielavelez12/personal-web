292fc98ced1ae4bdc2e66274fadbf86b4ea8dd9e
daniela <daniela@alza.app>
2023-07-18 23:50:02 -0400

    Fix apostrophes across all code

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ type Book = {
 }
 
 const books: Book[] = [
-  { title: "The Three-Body Problem", completed: false },
-  { title: "A Room of One&apos;s Own", completed: false },
-  { title: "The Death and Life of Great American Cities", completed: false },
   { title: "Art of Attack in Chess", completed: false },
+  { title: "A Room of One&apos;s Own", completed: false },
   { title: "Dictionary of Obscure Sorrows", completed: false },
-  { title: "Morning Brew", completed: false },
+  { title: "The Lincoln Highway", completed: true },
+  { title: "Atomic Habits", completed: true },
+  { title: "The Elephant Vanishes", completed: true },
+  { title: "The Three-Body Problem", completed: true },
+  { title: "The Death and Life of Great American Cities", completed: true },
   { title: "On Palestine", completed: true },
   { title: "A Gentleman in Moscow", completed: true },
   { title: "Cloud Cuckoo Land", completed: true },
@@ -27,18 +29,17 @@ const books: Book[] = [
   { title: "The Goal: A Process of Ongoing Improvement", completed: true },
   { title: "Antifragile", completed: true },
   { title: "Crying in H-Mart", completed: true },
-  { title: "Goodbye, Again", completed: true }
+  { title: "Goodbye, Again", completed: true },
 ];
-
 
 const Home = () => {
   return (
     <div className="app">
       <h1 className="title">Daniela Velez</h1>
       <p className="bio">
-        Hi! I'm Daniela and I'm currently a software engineer at <a href="https://alza.app/">Alza</a>,
+        Hi! I&apos;m Daniela and I&apos;m currently a software engineer at <a href="https://alza.app/">Alza</a>,
         a startup focused on empowering Latin Americans financially. I live in NYC and enjoy playing piano
-        and reading! Here's a list of the books I'm currently reading or have read recently:
+        and reading! Here&apos;s a list of the books I&apos;m currently reading or have read recently:
       </p>
       <ul className="reading-list">
         {books.map((book, index) => (
